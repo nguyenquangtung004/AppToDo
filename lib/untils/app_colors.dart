8be6a2dbd0adf61ba61bc2d845f3715d96658eb8
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2024-11-21 15:42:56 +0700

    Tạo ra 2 file khai báo màu sắc và chữ bên cạnh đó thêm file main code đã có sẵn

---
+++ lib/untils/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff73aeec);
+
+  static const List<Color> primaryGradientColor = [
+    Color(0xff4568dc),
+    Color(0xffb06ab3),
+  ];
+}
