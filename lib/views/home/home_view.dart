2e7c74ec677dbcb910d1a4d9bf0e9ebdbc8aa6ec
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2024-11-22 12:00:35 +0700

    Tách body thành method riêng

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -32,159 +32,169 @@ class _HomeViewState extends State<HomeView> {
       floatingActionButton: Fab(),
 
       //Body
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            ///Custom App Bar
-            Container(
-              margin: const EdgeInsets.only(top: 60),
-              width: double.infinity,
-              height: 100,
-              // color: Colors.red,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //Progress Indicator
-                  const SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: CircularProgressIndicator(
-                      value: 1 / 3,
-                      backgroundColor: Colors.grey,
-                      valueColor:
-                          AlwaysStoppedAnimation(AppColors.primaryColor),
+      body: _buildHomeBody(textTheme),
+    );
+  }
+
+  ///Home Body
+  Widget _buildHomeBody(TextTheme textTheme) {
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        children: [
+          ///Custom App Bar
+          Container(
+            margin: const EdgeInsets.only(top: 60),
+            width: double.infinity,
+            height: 100,
+            // color: Colors.red,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Progress Indicator
+                const SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircularProgressIndicator(
+                    value: 1 / 3,
+                    backgroundColor: Colors.grey,
+                    valueColor:
+                        AlwaysStoppedAnimation(AppColors.primaryColor),
+                  ),
+                ),
+                //Space
+                const SizedBox(
+                  width: 25,
+                ),
+                //Top Level Task Info
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      AppStr.mainTitle,
+                      style: textTheme.displayLarge,
                     ),
-                  ),
-                  //Space
-                  const SizedBox(
-                    width: 25,
-                  ),
-                  //Top Level Task Info
-                  Column(
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      "1 of 3 task",
+                      style: textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Divider(
+              thickness: 2,
+              indent: 100,
+            ),
+          ),
+
+          ///Tasks
+          SizedBox(
+            width: double.infinity,
+            height: 745,
+            child: tesing.isNotEmpty
+
+                /// Task list is not empty
+                ? ListView.builder(
+                    // padding: EdgeInsets.only(bottom: 80),
+                    itemCount: tesing.length,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      // Dismissible khi vuốt trái sang phải có thể xóa
+                      return Dismissible(
+                        direction: DismissDirection.horizontal,
+                        onDismissed: (direction) {
+                          setState(() {
+                            tesing.removeAt(index);
+                          });
+                        },
+                        background: Row(
+                          //mainAxisAlignment: MainAxisAlignment.center căn các lớp như icon và text ra giữa khi vuốt
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.delete_outline,
+                              color: Colors.grey,
+                            ),
+                            8.w,
+                            const Text(
+                              AppStr.deletedTask,
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        key: UniqueKey(),
+                        child: const TaskWidget(),
+                      );
+                    },
+                  )
+                /// Task list is empty
+                : Column(
+                    //Căn animation ra giữa
                     mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        AppStr.mainTitle,
-                        style: textTheme.displayLarge,
+                      //Phải tự import bằng tay lottie
+                      //Xong sau đó tạo ra file space_exs.dart
+                      //Khai báo vào trong đó lottieUrl
+                      //rồi lên testing để tảo ra danh sách rỗng kiểm tra xem đã thực hiện thành công lottie chưa
+                      //nếu Animate mà nó không rỗng
+
+                      //Thêm lớp FadeInUp để làm animation chuyển động chậm
+                      ///Lottie Anime
+                      FadeIn(
+                        child: SizedBox(
+                          //Thay đổi kích thước animation
+                          width: 200,
+                          height: 200,
+                          child: Lottie.asset(lottieUrl,
+                              animate: tesing.isNotEmpty ? false : true),
+                        ),
                       ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      Text(
-                        "1 of 3 task",
-                        style: textTheme.titleMedium,
-                      ),
+                      //Sub Text
+                      FadeInUp(
+                        from: 30,
+                        ///Đặt ra một câu hỏi đó là tại sao cứ phải dùng const trong một số lớp giao diện ví dụ như :
+                        ///child: const Text(AppStr.doneAllTask)
+                        /// -> Tối ưu hóa hiệu suất của ứng dụng khi mỗi lần flutter chạy lại và phải tạo lại instance mới của widget Text đó, mặc dù giá trị của nó không thay đổi.
+                        ///----------------------------------------------------------------------------------------
+                        /// Tiếp theo instance là gì ?
+                        /// Được hiểu là  một bản sao cụ thể của một đối tượng được tạo ra từ một lớp (class).
+                        /// Bạn có thể tưởng tượng lớp như là một bản thiết kế hoặc khuôn mẫu,
+                        /// và khi bạn tạo một instance, bạn đang tạo ra một đối tượng thực tế từ thiết kế đó.
+                        ///-----------------------------------------------------------------------------------------
+                        /// -> Có thể hiểu đơn giản qua ví dụ sau đây :
+                        /// * class Person {
+                        ///   String name;
+                        ///  int age;
+                        ///
+                        ///   Person({required this.name, required this.age});
+                        /// }
+                        /// *
+                        ///----------------------------------------------------------------------------------------
+                        /// -> Person là lớp (class),
+                        /// -> person1 và person2 là instance của lớp Person.
+                        /// -> Mỗi instance có thể có các giá trị riêng biệt (ví dụ name và age),
+                        /// mặc dù chúng đều được tạo từ lớp Person
+                        ///----------------------------------------------------------------------------------------
+                        ///Đơn giản hóa
+                        ///->Lớp: Text (mô tả cách hiển thị văn bản)
+                        ///->Instance: Cái cụ thể mà bạn đang hiển thị trên màn hình (ví dụ: Text("Hello World"))
+
+                        child: const Text(AppStr.doneAllTask),
+                      )
                     ],
                   ),
-                ],
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Divider(
-                thickness: 2,
-                indent: 100,
-              ),
-            ),
-
-            ///Tasks
-            SizedBox(
-              width: double.infinity,
-              height: 745,
-              child: tesing.isNotEmpty
-                  ? ListView.builder(
-                      // padding: EdgeInsets.only(bottom: 80),
-                      itemCount: tesing.length,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) {
-                        // Dismissible khi vuốt trái sang phải có thể xóa
-                        return Dismissible(
-                          direction: DismissDirection.horizontal,
-                          onDismissed: (direction) {
-                            setState(() {
-                              tesing.removeAt(index);
-                            });
-                          },
-                          background: Row(
-                            //mainAxisAlignment: MainAxisAlignment.center căn các lớp như icon và text ra giữa khi vuốt
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.delete_outline,
-                                color: Colors.grey,
-                              ),
-                              8.w,
-                              const Text(
-                                AppStr.deletedTask,
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                            ],
-                          ),
-                          key: UniqueKey(),
-                          child: const TaskWidget(),
-                        );
-                      },
-                    )
-                  : Column(
-                      //Căn animation ra giữa
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        //Phải tự import bằng tay lottie
-                        //Xong sau đó tạo ra file space_exs.dart
-                        //Khai báo vào trong đó lottieUrl
-                        //rồi lên testing để tảo ra danh sách rỗng kiểm tra xem đã thực hiện thành công lottie chưa
-                        //nếu Animate mà nó không rỗng
-
-                        //Thêm lớp FadeInUp để làm animation chuyển động chậm
-                        FadeIn(
-                          child: SizedBox(
-                            //Thay đổi kích thước animation
-                            width: 200,
-                            height: 200,
-                            child: Lottie.asset(lottieUrl,
-                                animate: tesing.isNotEmpty ? false : true),
-                          ),
-                        ),
-                        FadeInUp(
-                          from: 30,
-                          ///Đặt ra một câu hỏi đó là tại sao cứ phải dùng const trong một số lớp giao diện ví dụ như :
-                          ///child: const Text(AppStr.doneAllTask)
-                          /// -> Tối ưu hóa hiệu suất của ứng dụng khi mỗi lần flutter chạy lại và phải tạo lại instance mới của widget Text đó, mặc dù giá trị của nó không thay đổi.
-                          ///----------------------------------------------------------------------------------------
-                          /// Tiếp theo instance là gì ?
-                          /// Được hiểu là  một bản sao cụ thể của một đối tượng được tạo ra từ một lớp (class).
-                          /// Bạn có thể tưởng tượng lớp như là một bản thiết kế hoặc khuôn mẫu,
-                          /// và khi bạn tạo một instance, bạn đang tạo ra một đối tượng thực tế từ thiết kế đó.
-                          ///-----------------------------------------------------------------------------------------
-                          /// -> Có thể hiểu đơn giản qua ví dụ sau đây :
-                          /// * class Person {
-                          ///   String name;
-                          ///  int age;
-                          ///
-                          ///   Person({required this.name, required this.age});
-                          /// }
-                          /// *
-                          ///----------------------------------------------------------------------------------------
-                          /// -> Person là lớp (class),
-                          /// -> person1 và person2 là instance của lớp Person.
-                          /// -> Mỗi instance có thể có các giá trị riêng biệt (ví dụ name và age),
-                          /// mặc dù chúng đều được tạo từ lớp Person
-                          ///----------------------------------------------------------------------------------------
-                          ///Đơn giản hóa
-                          ///->Lớp: Text (mô tả cách hiển thị văn bản)
-                          ///->Instance: Cái cụ thể mà bạn đang hiển thị trên màn hình (ví dụ: Text("Hello World"))
-
-                          child: const Text(AppStr.doneAllTask),
-                        )
-                      ],
-                    ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
